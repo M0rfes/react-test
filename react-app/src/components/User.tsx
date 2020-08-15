a07aa723bf1847e951995fdc073b00b075312e42
M0rfes <fahimkhan20148@gmail.com>
2020-08-15 12:53:09 +0530

    ratting

--- react-app/src/components/User.tsx
+++ react-app/src/components/User.tsx
@@ -5,6 +5,7 @@ import FirebaseContext from '../firebase';
 type App = {
   [name: string]: {
     title: string;
+    ratting: number;
   };
 };
 const User: FC<{ id: string; user: TUser }> = ({ id, user }) => {
@@ -18,16 +19,50 @@ const User: FC<{ id: string; user: TUser }> = ({ id, user }) => {
         setApps(snap.val().apps);
       });
   }, [firebase]);
+  const doSubmit = (name: string, app: { title: string; ratting: number }) => {
+    firebase
+      .database()
+      .ref(`accounts/${user.account}/apps/${name}`)
+      .update(app)
+      .then((c) => console.log(c));
+  };
   return (
     <>
       <div>
         <p>name: {user.name}</p>
         <p>account: {user.account}</p>
         <strong>apps</strong>
-        {Object.values(apps).map((app, i) => (
-          <p key={i}>
-            app {i + 1}: {app.title}
-          </p>
+        {Object.entries(apps).map(([name, app], i) => (
+          <form
+            key={i}
+            onSubmit={(e) => {
+              e.preventDefault();
+              doSubmit(name, app);
+            }}
+          >
+            <p>
+              app {i + 1}: {app.title}
+            </p>
+            <input
+              type="number"
+              min="0"
+              max="5"
+              value={app.ratting}
+              onChange={(e) =>
+                setApps((apps) => {
+                  const app = apps[name];
+                  return {
+                    ...apps,
+                    [name]: {
+                      ...app,
+                      ratting: (e.target as any).value,
+                    },
+                  };
+                })
+              }
+            ></input>
+            <button type="submit">Rate</button>
+          </form>
         ))}
       </div>
       <hr />
